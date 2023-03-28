42d462f03eff84c45941c7c8031ee195bb5b04e1
namikaze-dev <usmansadik96@gmail.com>
2023-03-28 16:51:03 +0100

    Add shuffle questions option to main

--- main.go
+++ main.go
@@ -3,22 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 type Options struct {
-	Source string
+	Source  string
 	Timeout int
+	Shuffle bool
 }
 
 func main() {
 	var options Options
 	flag.StringVar(&options.Source, "file", "problems.csv", "source csv file for questions")
 	flag.IntVar(&options.Timeout, "timeout", 30, "quiz time limit")
+	flag.BoolVar(&options.Shuffle, "shuffle", false, "shuffle questions")
 	flag.Parse()
 
 	source := openFile(options.Source)
 	questions := ParseQuestionsFromReader(source)
+	if options.Shuffle {
+		shuffle(questions)
+	}
+
 	PlayGame(os.Stdout, os.Stdin, questions, options)
 }
 
@@ -29,4 +37,12 @@ func openFile(fn string) *os.File {
 		os.Exit(-1)
 	}
 	return f
+}
+
+func shuffle(questions []ParsedQuestion) {
+	rand.Seed(time.Now().UnixMilli())
+	for i := range questions {
+		j := rand.Intn(len(questions))
+		questions[i], questions[j] = questions[j], questions[i]
+	}
 }
